7920815e4060dafc356beff4836bf1d7b5b48ace
Kaltrina <kaltrinalubovci@outlook.com>
2019-07-06 01:57:13 +0200

    Android Project

---
+++ app/src/main/java/com/project/notebook/callbacks/NoteEventListener.java
@@ -0,0 +1,20 @@
+package com.project.notebook.callbacks;
+
+import com.project.notebook.model.Note;
+
+
+public interface NoteEventListener {
+    /**
+     * call wen note clicked.
+     *
+     * @param note: note item
+     */
+    void onNoteClick(Note note);
+
+    /**
+     * call wen long Click to note.
+     *
+     * @param note : item
+     */
+    void onNoteLongClick(Note note);
+}
